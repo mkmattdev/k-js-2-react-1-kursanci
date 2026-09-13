89ccf888f2db76100f970aca8b88e95666884273
Mateusz <mateusz@mattdev.com.pl>
2026-09-13 21:10:47 +0200

    add task for 1st lesson

---
+++ src/01-components/tasks/ShipmentCard.start.jsx
@@ -0,0 +1,20 @@
+////////
+//// Zadanie: karta przesyłki
+////////
+//
+// KONTEKST KOMERCYJNY
+// Magazyn chce widzieć przesyłkę na tej samej stronie co zamówienie, w tym samym układzie.
+//
+// CO MASZ ZROBIĆ
+// Napisz return z <article>: nagłówek <h3> i trzy akapity <p>, dane wpisane na sztywno.
+//
+// MA DZIAŁAĆ TAK
+//   PRZ-2207
+//   Klient: Robert Kowalski
+//   Kurier: InPost
+//   Waga: 1.2 kg
+
+export default function ShipmentCard() {
+  // TODO: return z <article>, w środku h3 i trzy <p>
+  return <p>Tutaj powstanie ShipmentCard</p>;
+}
