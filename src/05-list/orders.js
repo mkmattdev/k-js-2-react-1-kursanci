ef955481b928ce3b73de4486bbfd40200198923d
Mateusz <mateusz@mattdev.com.pl>
2026-09-22 13:51:18 +0200

    add structure for 05 lesson

---
+++ src/05-list/orders.js
@@ -0,0 +1,7 @@
+export const orders = [
+  { id: "ZAM-1042", customer: "Anna Nowak", totalPln: 6999.5, isPaid: true },
+  { id: "ZAM-1043", customer: "Robert Kowalski", totalPln: 79, isPaid: false },
+  { id: "ZAM-1044", customer: "Mateusz Lis", totalPln: 1249.9, isPaid: true },
+  { id: "ZAM-1045", customer: "Katarzyna Zięba", totalPln: 349, isPaid: false },
+  { id: "ZAM-1046", customer: "Piotr Wrona", totalPln: 129.99, isPaid: true },
+];
