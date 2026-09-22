045050b15d37202baed44238b368396a5396d18b
Mateusz <mateusz@mattdev.com.pl>
2026-09-22 12:54:00 +0200

    add structure for 04 lesson

---
+++ src/04-conditional/orders.js
@@ -0,0 +1,17 @@
+export const orders = [
+  {
+    id: "ZAM-1042",
+    customer: "Anna Nowak",
+    totalPln: 6999.5,
+    isPaid: true,
+    note: "Zostawić u sąsiada",
+  },
+  { id: "ZAM-1043", customer: "Robert Kowalski", totalPln: 79, isPaid: false },
+  {
+    id: "ZAM-1044",
+    customer: "Mateusz Lis",
+    totalPln: 1249.9,
+    isPaid: false,
+    note: "Dzwonić przed dostawą",
+  },
+];
